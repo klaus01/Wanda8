a65f2e39900753695659f79b57c62bbebfdaa44e
klaus01 <kelei0017@gmail.com>
2013-08-04 23:08:46 +0800

    抢票时在请求头中增加Referer，增强仿真度。

--- src/BaseWindaBrowser.pas
+++ src/BaseWindaBrowser.pas
@@ -27,6 +27,7 @@ implementation
 
 const
   C_CITYCODE = '4905168908';
+  C_QUERTYCITY_URL = 'http://app.wandafilm.com/wandaFilm/doqueryCitys.action?userId=%s';
   C_TICKET_URL = 'http://app.wandafilm.com/wandaFilm/ticket.action?cityCode=%s%s&userId=%s';
 
 {$R *.dfm}
@@ -51,11 +52,18 @@ end;
 
 procedure TfrmBaseWindaBrowser.FormShow(Sender: TObject);
 var
+  vFlags, vHeaders, vTargetFrameName, vPostData: OLEVariant;
+  vRef: string;
   vUrl, vRandomStr: string;
 begin
   vRandomStr := GetRandomNumberStr(51);
   vUrl := Format(C_TICKET_URL, [vRandomStr, C_CITYCODE, UserID]);
-  wb.Navigate(vUrl);
+  vFlags := '1';
+  vTargetFrameName := '';
+  vPostData := '';
+  vRef := Format(C_QUERTYCITY_URL, [UserID]);
+  vHeaders := 'Referer: ' + vRef + #13#10;
+  wb.Navigate(vUrl, vFlags, vTargetFrameName, vPostData, vHeaders);
 end;
 
 function TfrmBaseWindaBrowser.GetWebDoc: IHTMLDocument2;
